07ff22178d6d6f6ca01bf83fd12476e987622739
Luis Alfonso Mercado Manzo <118950279+luismercado5@users.noreply.github.com>
2023-04-18 17:33:58 -0600

    Add files via upload

--- practica6,2.py
+++ practica6,2.py
@@ -9,57 +9,56 @@ from PIL import Image, ImageTk
 import threading
 import tkinter as tk
 
-# Load the images
+# carga las imagenes
 image1 = Image.open("picture2.jpg")
 image2 = Image.open("picture1.jpg")
 
-# Create the tkinter window
+# Crea una ventana
 window = tk.Tk()
-window.geometry("1200x720")
+window.geometry("1800x1220")
 
-# Create the tkinter canvas
-canvas = tk.Canvas(window, width=1200, height=700)
+# Crea el tamaño de la ventana
+canvas = tk.Canvas(window, width=1270, height=720)
 canvas.pack()
 
-# Create the tkinter image objects
+# Crea el objeto imagen
 tk_image1 = ImageTk.PhotoImage(image1)
 tk_image2 = ImageTk.PhotoImage(image2)
 
-# Create the tkinter image objects on the canvas
-canvas_image1 = canvas.create_image(0, 500, image=tk_image1)
-canvas_image2 = canvas.create_image(500, 0, image=tk_image2)
+canvas_image1 = canvas.create_image(100, 500, image=tk_image1)
+canvas_image2 = canvas.create_image(500, 100, image=tk_image2)
 
-# Define the function to move image1 right to left
+# Define la funcion del movimiento de la imagen 1 izq a der
 def move_image1():
     x = 0
-    y = 300
+    y = 200
     while True:
         canvas.move(canvas_image1, 5, 0)
         window.update()
         x += 5
-        if x >= 600:
+        if x >= 800:
             x = 0
             canvas.coords(canvas_image1, x, y)
 
-# Define the function to move image2 up to down
+# Define la funcion de la imagen 2 arriba a abajo
 def move_image2():
-    x = 300
+    x = 200
     y = 0
     while True:
         canvas.move(canvas_image2, 0, 5)
         window.update()
         y += 5
-        if y >= 600:
+        if y >= 800:
             y = 0
             canvas.coords(canvas_image2, x, y)
 
-# Create the threads
+# Crea un hilo
 thread1 = threading.Thread(target=move_image1)
 thread2 = threading.Thread(target=move_image2)
 
-# Start the threads
+# inicia un hilo
 thread1.start()
 thread2.start()
 
-# Start the tkinter main loop
+# inicia el loop de tkinter
 window.mainloop()
